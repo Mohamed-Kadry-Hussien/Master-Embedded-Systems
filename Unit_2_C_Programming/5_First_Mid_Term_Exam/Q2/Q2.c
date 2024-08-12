6880b0426a72d0081f2e4713fda048e8d008e0cf
mohamed <mokadry2004@gmail.com>
2024-08-12 19:28:13 +0300

    Unit_2_C_Programming

---
+++ Unit_2_C_Programming/5_First_Mid_Term_Exam/Q2/Q2.c
@@ -0,0 +1,34 @@
+/*
+ ======================================================================================================================
+ Name        :  * -------------------------------------------------------
+                * Question Number: 2
+                * Required: c function to take an integer number and calculate it's square root
+                * ex1: input:4 ->output:2.000
+                * ex2: input:10 ->output:3.162
+ Author      : Eng.Mohamed Kadry Hussien
+ Created on	 : 12/8/2024
+ Description : Unit 2 C-Programming >>MidTerm using ANSI-style (c99)
+ ======================================================================================================================
+ */
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+
+float square_root(int num);  //prototype(deceleration)
+
+int main()
+{
+    int num;
+    printf("Enter the number : ");
+    scanf("%d",&num);
+    float root ;
+    root= square_root(num);
+    printf("the square root of (%d) equal %0.3f",num,root);
+}
+
+float square_root(int num)  //definition
+{
+    float square =sqrt(num) ;
+    return square;
+}
---
+++ Unit_2_C_Programming/5_First_Mid_Term_Exam/Q3/Q3.c
@@ -0,0 +1,54 @@
+/*
+ ======================================================================================================================
+ Name        :  * -------------------------------------------------------
+                * Question Number: 3
+                * Required: C function to take two integer numbers and find all prime numbers between them
+                * ex: input: 1, 20 -> output:1 2 5 7 11 13 17 19
+ Author      : Eng. Mohamed Kadry Hussien
+ Created on	 : 12/8/2024
+ Description : Unit 2 C-Programming >> MidTerm using ANSI-style (c99)
+ ======================================================================================================================
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+// Function prototype (declaration) for prime_Numbers
+void prime_Numbers(int num_1, int num_2);
+
+int main()
+{
+    int num_1, num_2;
+    printf("Enter the first number : ");
+    scanf("%d", &num_1);
+    printf("Enter the second number : ");
+    scanf("%d", &num_2);
+    printf("The prime numbers between (%d) and (%d) : \n", num_1, num_2);
+    prime_Numbers(num_1, num_2);
+
+    return 0;
+}
+
+// Function definition for prime_Numbers
+void prime_Numbers(int num_1, int num_2)
+{
+    int test = 0;
+
+    // Loop through each number from num_1 to num_2
+    for(int i = num_1; i <= num_2; i++)
+    {
+        // Reset test counter for each number
+        test = 0;
+        for(int j = 1; j <= i; j++)
+        {
+            if(i % j == 0)  // If j is a divisor of i
+            {
+                test++;
+            }
+        }
+        // A prime number has exactly 2 divisors: 1 and itself
+        if(test == 2 || test == 1)
+        {
+            printf("%d\t", i);  // Print the prime number
+        }
+    }
+}
