b1ee0e07b281db684ce10ffc7cd8914581dbba7f
mohamed <mokadry2004@gmail.com>
2024-08-12 16:50:45 +0300

    Unit_2_C_Programming

---
+++ Unit_2_C_Programming/5_First_Mid_Term_Exam/Q1/Q1.c
@@ -0,0 +1,38 @@
+/*
+ ======================================================================================================================
+ Name        :
+ Author      : Eng.Mohamed Kadry Hussien
+ Created on	 : 12/8/2024
+ Description : Unit 2 C-Programming >>MidTerm using ANSI-style (c99)
+ ======================================================================================================================
+ */
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+
+int sum_of_digits(int num);  //prototype(deceleration)
+
+int main()
+{
+    int num;
+    printf("Enter the number : ");
+    scanf("%d",&num);
+    int sum = sum_of_digits(num);
+    printf("the sum of digits in (%d) equal %d",num,sum);
+}
+int sum_of_digits(int num)  //definition
+{
+    static unsigned int sum =0; // to save last value of variable (sum) when call function again
+    int last_number;
+
+    if(num!=0)
+    {
+        int last_number=num%10 ;
+        sum+=last_number;
+        sum_of_digits(num/10);
+        return sum ;
+    }
+    else
+        return 0;
+}
