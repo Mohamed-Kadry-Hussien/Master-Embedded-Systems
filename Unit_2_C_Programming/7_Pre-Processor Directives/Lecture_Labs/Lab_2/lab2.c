5935dba0b46123eb8c1b16481f50eb3d31c4779a
Mohamed Kadry <mokadry2004@gmail.com>
2024-08-21 20:09:16 +0300

    Signed-off-by: Mohamed Kadry <mokadry2004@gmail.com>

---
+++ Unit_2_C_Programming/7_Pre-Processor Directives/Lecture_Labs/Lab_2/lab2.c
@@ -0,0 +1,78 @@
+/*
+======================================================================================================================
+ Name        : Lab_2
+ Author      : Eng.Mohamed Kadry Hussien
+ Created on	 : 21/8/2024
+ Description : Unit 2 C-Progtamming >>Pre-Processor Directives>>Lab_2  using Ansi-style (c99)
+ ======================================================================================================================
+ */
+#include <stdio.h>
+#define debug
+#define Dprintf(debug_level,...)   if (debug_level>1)\
+                                      printf("File Name : %s ,Function Name : %s ,Line Number : %d ",__FILE__,__func__,__LINE__); \
+                                      printf(">>\t");\
+                                      printf(__VA_ARGS__);
+int add(int a, int b)
+{
+#ifdef debug
+    Dprintf(2,"start function\n")
+#endif
+int sum = 0;
+    sum = a+b;
+    printf("sum is %d\n",sum);
+#ifdef debug
+    Dprintf(2,"exit function \n");
+#endif
+
+}
+int sub(int a, int b)
+{
+#ifdef debug
+    Dprintf(2,"start function \n");
+#endif
+int sum = 0;
+    sum=a-b;
+    printf("sum is %d\n",sum);
+#ifdef debug
+    Dprintf(2,"exit function \n");
+#endif
+}
+int mul(int a, int b)
+{
+#ifdef debug
+    Dprintf(2,"start function \n")
+#endif
+    int sum = 0;
+    sum=a*b;
+    printf("sum is %d\n",sum);
+    #ifdef debug
+    Dprintf(2,"exit function \n");
+    #endif
+}
+int div(int a, int b )
+{
+#ifdef debug
+    Dprintf(2,"start function\n")
+#endif
+    int sum = 0;
+    sum=a/b;
+    printf("sum is %d\n",sum);
+    #ifdef debug
+    Dprintf(2,"exit function \n");
+    #endif
+}
+int main()
+{
+#ifdef debug
+    Dprintf(2,"start function\n")
+#endif
+    add(12,12);
+    sub(24,12);
+    mul(25,12);
+    div(25,5);
+#ifdef debug
+    Dprintf(2,"exit function \n");
+#endif
+    return 0;
+
+}
---
+++ Unit_2_C_Programming/7_Pre-Processor Directives/Lecture_Labs/Lab_3/lab3.c
@@ -0,0 +1,21 @@
+/*
+======================================================================================================================
+ Name        : Lab_3
+ Author      : Eng.Mohamed Kadry Hussien
+ Created on	 : 21/8/2024
+ Description : Unit 2 C-Progtamming >>Pre-Processor Directives>>Lab_3  using Ansi-style (c99)
+ ======================================================================================================================
+ */
+#include <stdio.h>
+#define Fun(name,a)  int fun_##name(int x) {return a*x;}
+Fun(Double,2)
+Fun(Triple,3)
+Fun(Quadrable,4)
+int main()
+{
+    printf("Double(5) = %d\n",fun_Double(5));
+    printf("Triple(5) = %d\n",fun_Triple(5));
+    printf("Quadrable(5) = %d\n",fun_Quadrable(5));
+    return 0;
+
+}
