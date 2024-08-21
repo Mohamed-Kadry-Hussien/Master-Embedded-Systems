ad88e22aa34d196f5179aa328b73bd04f4658c66
Mohamed Kadry <mokadry2004@gmail.com>
2024-08-21 23:40:25 +0300

    Signed-off-by: Mohamed Kadry <mokadry2004@gmail.com>

---
+++ Unit_2_C_Programming/7_Pre-Processor Directives/Lecture_Labs/Lab_4/lab4.c
@@ -0,0 +1,43 @@
+/*
+======================================================================================================================
+ Name        : Lab_3
+ Author      : Eng.Mohamed Kadry Hussien
+ Created on	 : 21/8/2024
+ Description : Unit 2 C-Progtamming >>Pre-Processor Directives>>Lab_3  using Ansi-style (c99)
+ ======================================================================================================================
+*/
+
+#include <stdio.h>
+void usa();
+void india();
+
+// Method one
+//#pragma startup india 105
+//#pragma startup usa
+//#pragma exit usa
+//#pragma exit india 105
+
+
+// Method two
+
+void __attribute__ ((constructor)) usa(); // == startup
+void __attribute__ ((constructor)) india();
+void __attribute__ ((destructor)) usa(); // == exit
+void __attribute__ ((destructor)) india(); // == exit
+
+
+int main(void)
+{
+    printf("I'm in main\n");
+    return 0;
+}
+
+void usa ()
+{
+    printf("I'm in usa\n");
+}
+
+void india ()
+{
+    printf("I'm in india\n");
+}
