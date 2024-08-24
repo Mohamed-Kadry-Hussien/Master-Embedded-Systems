4eec6e996efaeec6e74b90a38f704ad34f45043a
Mohamed Kadry <mokadry2004@gmail.com>
2024-08-24 23:21:45 +0300

    add labs of pointer Unit_2_C_Programming

---
+++ Unit_2_C_Programming/8_Pointers/Lecture_Labs/Lab_1.c
@@ -0,0 +1,31 @@
+/*
+======================================================================================================================
+ Name        : Lab_1_Average of Weight.
+ Author      : Eng.Mohamed Kadry Hussien
+ Created on	 : 24/8/2024
+ Description : Unit 2 C-Programming >>Pointer>>Lab_1 using ANSI C-style (c99)
+ ======================================================================================================================
+ */
+#include <stdio.h>
+#define  size 5
+int main()
+{
+    int arr[size];
+    int sum =0;
+    int i;
+    int *Parr=arr;
+    printf("Please enter the elements\n");
+    for(i=0;i<size;i++)
+        scanf("%d",Parr+i);
+    puts("");
+    int j;
+    printf("the elements are:\n");
+    for (j=0;j<size;j++)
+        printf("%d\t",*Parr+j);
+    puts("");
+    int k;
+    for(k=0;k<size;k++,Parr++)
+        sum+=*Parr;
+    printf("the sum = %d",sum);
+    return 0;
+}
