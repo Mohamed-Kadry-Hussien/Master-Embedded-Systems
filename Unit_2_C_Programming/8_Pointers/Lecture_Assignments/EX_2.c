8647c684815344967ac3104db10156361d7cb7d7
Mohamed Kadry <mokadry2004@gmail.com>
2024-08-28 18:53:57 +0300

    add Assignment of Pointer Unit_2_C_Programming

---
+++ Unit_2_C_Programming/8_Pointers/Lecture_Assignments/EX_2.c
@@ -0,0 +1,35 @@
+/*
+======================================================================================================================
+ Name        : EX2_
+ Author      : Eng.Mohamed Kadry Hussien
+ Created on	 : 27/8/2024
+ Description : Unit 2 C-Programming >>Pointer>>Assignments>>EX_2 using ANSI-style (c99)
+ ======================================================================================================================
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    int alph[27];
+    int* ptr;
+    ptr=alph;       /*pointer point to first element of array*/
+    int i;
+    for(i=0;i<26;i++)
+    {
+        *ptr='A'+i ;
+        ptr++;      /* equivalent to (ptr = ptr + size of data type(ptr) */
+    }
+    ptr=alph;       /*return pointer  point to first element of array*/
+    printf("The Alphabets are : ");
+
+    for(i=0;i<26;i++)
+    {
+      printf("%C\t",*ptr);
+      ptr++;
+    }
+
+
+
+  return 0;
+}
