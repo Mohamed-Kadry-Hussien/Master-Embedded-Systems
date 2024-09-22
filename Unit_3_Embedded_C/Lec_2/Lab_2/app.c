578cbf22ffbb55b5a982eb734a9b8a61e821a0ae
MOHAMED KADRY HUSSIEN <143326933+Mohamed-Kadry-Hussien@users.noreply.github.com>
2024-09-23 00:18:18 +0300

    Delete Unit_3_Embedded_C/Lec_2 directory

--- Unit_3_Embedded_C/Lec_2/Lab_2/app.c
+++
@@ -1,8 +0,0 @@
-/*************app.c**************/
-#include "uart.h"
-unsigned char string_buffer[100]="Learn-in-Depth:<Mohamed Kadry>";
-void main(void)
-{
- //VersatilepB Physical Board
- Uart_Send_String(string_buffer); //send first element of array
-}
--- Unit_3_Embedded_C/Lec_2/Lab_2/startup.s
+++
@@ -1,7 +0,0 @@
-/***************Startup***************/
-.globl reset
-reset :
-ldr sp , =stack_top
-bl main 
-stop : 
-b stop
